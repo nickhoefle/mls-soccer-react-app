f11ded82766bb3a3ba0a18049bd3a7c2bde1eb04
Nick <nickhoefle@gmail.com>
2023-06-16 23:58:14 -0500

    Added horizontal scrolling to team roster/stats table in mobile while maintaining md/lg appearance.

--- src/Components/TeamRoster.jsx
+++ src/Components/TeamRoster.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React from 'react';
 import data from '../data/player-standard-stats.json';
 
 const TeamRoster = ({ team, setPlayer }) => {
-
     const teamPlayers = data.filter((player) => player.Squad === team);
 
     const handlePlayerClick = (playerName) => {
@@ -10,12 +9,13 @@ const TeamRoster = ({ team, setPlayer }) => {
     };
 
     return (
-        <>
-        <div className='bg-black opacity-75 flex justify-center pb-10'>
-            <table className="table-auto text-white">
-                <thead>
-                    <tr>
-                        <th className="px-4 py-2">Player</th>
+        <div className="overflow-x-auto">
+            <div className="bg-black bg-opacity-75 pb-10">
+                <div className="md:flex md:justify-center md:items-center">
+                <table className="table-auto text-white text-sm md:text-base">
+                    <thead>
+                    <tr className='md:opacity-75'>
+                        <th className="px-4 py-2 sticky left-0 bg-black">Player</th>
                         <th className="px-4 py-2">Age</th>
                         <th className="px-4 py-2">Position</th>
                         <th className="px-4 py-2">Matches</th>
@@ -25,11 +25,16 @@ const TeamRoster = ({ team, setPlayer }) => {
                         <th className="px-4 py-2">Assists</th>
                         <th className="px-4 py-2">Yellow Cards</th>
                     </tr>
-                </thead>
-                <tbody>
-                {teamPlayers.map((player) => (
-                    <tr key={player.Player} className='hover:bg-green-700'>
-                        <td className="border px-4 py-2 text-left" onClick={() => handlePlayerClick(player.Player)}>{player.Player}</td>
+                    </thead>
+                    <tbody>
+                    {teamPlayers.map((player) => (
+                        <tr key={player.Player} className="md:hover:bg-green-700 bg-black md:opacity-75">
+                        <td
+                            className="border px-4 py-2 sticky left-0 whitespace-nowrap bg-black md:bg-transparent cursor-pointer"
+                            onClick={() => handlePlayerClick(player.Player)}
+                        >
+                            {player.Player}
+                        </td>
                         <td className="border px-4 py-2 text-center">{player.Age.split('-')[0]}</td>
                         <td className="border px-4 py-2 text-center">{player.Pos}</td>
                         <td className="border px-4 py-2 text-center">{player.MP}</td>
@@ -38,13 +43,14 @@ const TeamRoster = ({ team, setPlayer }) => {
                         <td className="border px-4 py-2 text-center">{player.Gls}</td>
                         <td className="border px-4 py-2 text-center">{player.Ast}</td>
                         <td className="border px-4 py-2 text-center">{player.CrdY}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                </div>
+            </div>
         </div>
-        </>
     );
 }
 
-export default TeamRoster
+export default TeamRoster;
