e23295c72841a311844d067ef5834925fab647db
Nick <nickhoefle@gmail.com>
2023-08-09 16:35:27 -0500

    Added dataCleaners js to LeagueLeadersSwiper.jsx; reformatted code; changed variable names for clarity

--- src/Components/LeagueLeadersSwiper.jsx
+++ src/Components/LeagueLeadersSwiper.jsx
@@ -4,8 +4,15 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "../styles.css";
 import { Pagination, Navigation } from "swiper";
-import data from '../data/leaguePlayersStandardStatsRawData.json';
-import goalkeeping from '../data/leagueGoalkeeperStatsRawData.json';
+
+import leaguePlayersStandardStatsRawData from '../data/leaguePlayersStandardStatsRawData.json';
+import { cleanStandardStats } from '../dataCleaner'; 
+
+import leagueGoalkeeperStatsRawData from '../data/leagueGoalkeeperStatsRawData.json';
+import { cleanGoalkeeperStats } from '../dataCleaner';
+
+const leaguePlayersStandardStats = cleanStandardStats(leaguePlayersStandardStatsRawData);
+const leagueGoalkeeperStats = cleanGoalkeeperStats(leagueGoalkeeperStatsRawData);
 
 const LeagueLeadersSwiper = ({ setPlayer }) => {
 
@@ -18,28 +25,36 @@ const LeagueLeadersSwiper = ({ setPlayer }) => {
         return sortedData.slice(0,10);
     };
 
-    const topFiveAssists = findTopTenForStat(data, 'Ast'); 
-    const topFiveGoals = findTopTenForStat(data, 'Gls'); 
-    const topFiveSavePct = findTopTenForStat(goalkeeping, 'Save%'); 
+    const topFiveAssists = findTopTenForStat(leaguePlayersStandardStats, 'Assists'); 
+    const topFiveGoals = findTopTenForStat(leaguePlayersStandardStats, 'Goals'); 
+    const topFiveSavePct = findTopTenForStat(leagueGoalkeeperStats, 'SavePercent'); 
 
     return (
         <>
             <h1 className='text-center text-2xl text-white pb-4'>Top Performers</h1>
             <Swiper
-            pagination={{
-                type: "progressbar",
-            }}
-            navigation={false}
-            modules={[Pagination, Navigation]}
-            style={{ height: "340px" }}
+                pagination={{
+                    type: "progressbar",
+                }}
+                navigation={false}
+                modules={[Pagination, Navigation]}
+                style={{ height: "340px" }}
             >
                 <SwiperSlide className="bg-black text-white text-base px-2 pt-4">
                     <h1 className='underline pb-2'>Assists</h1>
                     <ul className="space-y-1.5">
-                        {topFiveAssists.map((player, index) => (
-                        <li key={index} className='flex items-center justify-between' onClick={() => handlePlayerClick(player.Player)}>
-                            {player.Player} ({player.Squad})
-                            <span className='pl-4 text-right'>{player.Ast}</span>
+                        {topFiveAssists.map((topFivePlayer, index) => (
+                        <li 
+                            key={index} 
+                            className='flex items-center justify-between' 
+                            onClick={() => handlePlayerClick(topFivePlayer.Name)}
+                        >
+                            {topFivePlayer.Name} ({topFivePlayer.Team})
+                        <span 
+                            className='pl-4 text-right'
+                        >
+                            {topFivePlayer.Assists}
+                        </span>
                         </li>
                         ))}
                     </ul>
@@ -47,10 +62,18 @@ const LeagueLeadersSwiper = ({ setPlayer }) => {
                 <SwiperSlide className="bg-black text-white text-base px-2 pt-4">
                     <h1 className='underline pb-2'>Goals</h1>
                     <ul className="space-y-1.5">
-                        {topFiveGoals.map((player, index) => (
-                        <li key={index} className='flex items-center justify-between' onClick={() => handlePlayerClick(player.Player)}>
-                            {player.Player} ({player.Squad})
-                            <span className='pl-4 text-right'>{player.Gls}</span>
+                        {topFiveGoals.map((topFivePlayer, index) => (
+                        <li 
+                            key={index} 
+                            className='flex items-center justify-between' 
+                            onClick={() => handlePlayerClick(topFivePlayer.Name)}
+                        >
+                            {topFivePlayer.Name} ({topFivePlayer.Team})
+                        <span 
+                            className='pl-4 text-right'
+                        >
+                            {topFivePlayer.Goals}
+                        </span>
                         </li>
                         ))}
                     </ul>
@@ -58,10 +81,17 @@ const LeagueLeadersSwiper = ({ setPlayer }) => {
                 <SwiperSlide className="bg-black text-white text-base px-2 pt-4">
                     <h1 className='underline pb-2'>Save Percent</h1>
                     <ul className="space-y-1.5">
-                        {topFiveSavePct.map((player, index) => (
-                        <li key={index} className='flex items-center justify-between'>
-                            {player.Player} ({player.Squad})
-                            <span className='pl-4 text-right'>{player['Save%'].toFixed(1)} %</span>
+                        {topFiveSavePct.map((topFivePlayer, index) => (
+                        <li 
+                            key={index} 
+                            className='flex items-center justify-between'
+                        >
+                            {topFivePlayer.Name} ({topFivePlayer.Team})
+                        <span 
+                            className='pl-4 text-right'
+                        >
+                            {topFivePlayer.SavePercent.toFixed(1)} %
+                        </span>
                         </li>
                         ))}
                     </ul>           
