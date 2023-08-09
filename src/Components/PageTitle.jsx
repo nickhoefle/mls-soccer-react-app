a68a9663f4101d5e9a4a97f479ce29825f956b45
Nick <nickhoefle@gmail.com>
2023-08-09 16:36:28 -0500

    Minor reformatting on PageTitle.jsx.

--- src/Components/PageTitle.jsx
+++ src/Components/PageTitle.jsx
@@ -4,9 +4,11 @@ const PageTitle = () => {
     return (
         <div className="bg-black p-2 flex justify-center">
             <a 
-            className="text-white text-center text-4xl"
-            href='/'
-            >MLStats by Nick</a>
+                className="text-white text-center text-4xl"
+                href='/'
+            >
+                MLStats by Nick
+            </a>
         </div>
     )
 }
