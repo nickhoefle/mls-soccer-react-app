e125c4a64408a367c59b6ea82e259248fbda661c
Nick <nickhoefle@gmail.com>
2023-09-15 15:43:56 -0500

    Changed external hrefs to local srcs for crests on team pages.

--- src/teamLogoHelper.js
+++ src/teamLogoHelper.js
@@ -1,63 +1,63 @@
-export function getTeamLogoHref(team) {
+export function getTeamLogoSrc(team) {
     switch (team) {
         case 'Austin':
-            return 'https://www.thesportsdb.com/images/media/team/badge/a3dlg61595434277.png';
+            return '/logos/full-size/austin.png';
         case 'Colorado Rapids':
-            return 'https://www.thesportsdb.com/images/media/team/badge/uppupv1473536412.png';
+            return '/logos/full-size/colorado.png';
         case 'FC Dallas':
-            return 'https://www.thesportsdb.com/images/media/team/badge/vxy8xy1602103187.png';
+            return '/logos/full-size/dallas.png';
         case 'Dynamo FC':
-            return 'https://www.thesportsdb.com/images/media/team/badge/em146s1606772337.png';
+            return '/logos/full-size/houston.png';
         case 'LA Galaxy':
-            return 'https://www.thesportsdb.com/images/media/team/badge/ysyysr1420227188.png';
+            return '/logos/full-size/lagalaxy.png';
         case 'Los Angeles FC':
-            return 'https://www.thesportsdb.com/images/media/team/badge/7nbj2a1602103638.png';
+            return '/logos/full-size/lafc.png';
         case 'Minnesota Utd':
-            return 'https://www.thesportsdb.com/images/media/team/badge/vl67t41492453433.png';
+            return '/logos/full-size/minnesota.png';
         case 'Portland Timbers':
-            return 'https://www.thesportsdb.com/images/media/team/badge/skm30j1557953559.png';
+            return '/logos/full-size/portland.png';
         case 'Real Salt Lake':
-            return 'https://www.thesportsdb.com/images/media/team/badge/kkjlfa1556488022.png';
+            return '/logos/full-size/rsl.png';
         case 'San Jose':
-            return 'https://www.thesportsdb.com/images/media/team/badge/xyrqqt1420781048.png';
+            return '/logos/full-size/sanjose.png';
         case 'Seattle':
-            return 'https://www.thesportsdb.com/images/media/team/badge/vpyprt1473536493.png';
+            return '/logos/full-size/seattle.png';
         case 'Sporting KC':
-            return 'https://www.thesportsdb.com/images/media/team/badge/tqupxw1473536504.png';
+            return '/logos/full-size/skc.png';
         case 'St. Louis':
-            return 'https://www.thesportsdb.com/images/media/team/badge/22wanp1674118724.png';
+            return '/logos/full-size/stlouis.png';
         case 'Vancouver':
-            return 'https://www.thesportsdb.com/images/media/team/badge/tpwxpy1473536521.png';
+            return '/logos/full-size/vancouver.png';
         case 'Atlanta Utd':
-            return 'https://www.thesportsdb.com/images/media/team/badge/ej091x1602103070.png';
+            return '/logos/full-size/atlanta.png';
         case 'Charlotte':
-            return 'https://www.thesportsdb.com/images/media/team/badge/b6p4uz1595434047.png';
+            return '/logos/full-size/charlotte.png';
         case 'Chicago Fire':
-            return 'https://www.thesportsdb.com/images/media/team/badge/8xuc781639493166.png';
+            return '/logos/full-size/chicago.png';
         case 'Columbus Crew':
-            return 'https://www.thesportsdb.com/images/media/team/badge/dzs8cp1629059854.png';
+            return '/logos/full-size/columbus.png';
         case 'FC Cincinnati':
-            return 'https://www.thesportsdb.com/images/media/team/badge/47ve8t1555084948.png';
+            return '/logos/full-size/cincinnati.png';
         case 'D.C. United':
-            return 'https://www.thesportsdb.com/images/media/team/badge/uwvsyt1467462609.png';
+            return '/logos/full-size/dc.png';
         case 'Inter Miami':
-            return 'https://www.thesportsdb.com/images/media/team/badge/m4it3e1602103647.png';
+            return '/logos/full-size/miami.png';
         case 'CF Montréal':
-            return 'https://www.thesportsdb.com/images/media/team/badge/livdl31679344925.png';
+            return '/logos/full-size/montreal.png';
         case 'Nashville':
-            return 'https://www.thesportsdb.com/images/media/team/badge/znrwt71602103062.png';
+            return '/logos/full-size/nashville.png';
         case 'New England':
-            return 'https://www.thesportsdb.com/images/media/team/badge/1ula2l1639493143.png';
+            return '/logos/full-size/newengland.png';
         case 'NYCFC':
-            return 'https://www.thesportsdb.com/images/media/team/badge/li4y2o1534575820.png';
+            return '/logos/full-size/nycfc.png';
         case 'NY Red Bulls':
-            return 'https://www.thesportsdb.com/images/media/team/badge/suytvy1473536462.png';
+            return '/logos/full-size/nyredbulls.png';
         case 'Orlando City':
-            return 'https://www.thesportsdb.com/images/media/team/badge/qyppxw1423832326.png';
+            return '/logos/full-size/orlando.png';
         case 'Philadelphia':
-            return 'https://www.thesportsdb.com/images/media/team/badge/gyznyo1602103682.png';
+            return '/logos/full-size/philadelphia.png';
         case 'Toronto':
-            return 'https://www.thesportsdb.com/images/media/team/badge/rsxyrr1473536512.png';
+            return '/logos/full-size/toronto.png';
         default:
             return 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/MLS_crest_logo_RGB_gradient.svg/1024px-MLS_crest_logo_RGB_gradient.svg.png'
     }
