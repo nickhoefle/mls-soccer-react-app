79c9508b303bdc6e441cce619cd9dcc2c71943d6
Nick Hoefle <Nickhoefle@gmail.com>
2023-09-16 23:30:42 -0500

    Correct records rendering on team pages

---
+++ src/teamRecord.js
@@ -0,0 +1,34 @@
+import allLeagueMatchesRawData from './data/allLeagueMatchesRawData.json';
+import { cleanAllLeagueMatchesRawData } from './dataCleaner'; 
+
+const allLeagueMatches = cleanAllLeagueMatchesRawData(allLeagueMatchesRawData);
+
+export function teamRecord(team) {
+    
+    const teamMatchesArray = allLeagueMatches.filter((match) => (match.HomeTeam === team || match.AwayTeam === team) && match.Score !== "")
+
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+
+    teamMatchesArray.forEach((match) => {
+        const isHomeTeam = match.HomeTeam === team;
+        const isAwayTeam = match.AwayTeam === team;
+        const scoreParts = match.Score.split('–');
+        const homeScore = parseInt(scoreParts[0]);
+        const awayScore = parseInt(scoreParts[1]);
+
+        if (isHomeTeam && homeScore > awayScore) {
+            wins++;
+        } else if (isAwayTeam && awayScore > homeScore) {
+            wins++;
+        } else if (homeScore === awayScore) {
+            ties++;
+        } else { 
+            losses++; 
+        }
+    });
+
+    return `${wins} - ${losses} - ${ties}`;
+
+}
