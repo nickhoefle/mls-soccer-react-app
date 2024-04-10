a42e991b4389dd1ac77e528cce136b51288f9fdd
Nick <nickhoefle@gmail.com>
2024-04-09 21:02:42 -0500

    Replaced linked logo strip icons with files in repo.

--- src/Components/TeamLogoStrip.jsx
+++ src/Components/TeamLogoStrip.jsx
@@ -10,176 +10,234 @@ const TeamLogoStrip = ({ handleTeamSelect }) => {
     return (
         <div className='flex flex-wrap justify-center bg-black'>
             <Link to='/team/AustinFC'>
-                <img src='https://www.thesportsdb.com/images/media/team/badge/a3dlg61595434277.png/tiny' 
-                onClick={() => handleClick('Austin')}
+                <img
+                    className='team-logo'
+                    src='/logos/full-size/austin.png' 
+                    onClick={() => handleClick('Austin')}
                 />
             </Link>
             
             <Link to='/team/ColoradoRapids'>    
-                <img src='https://www.thesportsdb.com/images/media/team/badge/uppupv1473536412.png/tiny'
-                onClick={() => handleClick('Colorado Rapids')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/colorado.png'
+                    onClick={() => handleClick('Colorado Rapids')}
                 />
             </Link>   
             
             <Link to='/team/FCDallas'>      
-                <img src='https://www.thesportsdb.com/images/media/team/badge/vxy8xy1602103187.png/tiny' 
-                onClick={() => handleClick('FC Dallas')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/dallas.png'
+                    onClick={() => handleClick('FC Dallas')}
                 />
             </Link>    
 
             <Link to='/team/HoustonDynamo'> 
-                <img src='https://www.thesportsdb.com/images/media/team/badge/em146s1606772337.png/tiny'
-                onClick={() => handleClick('Dynamo FC')} 
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/houston.png'
+                    onClick={() => handleClick('Dynamo FC')} 
                 />
             </Link>    
                 
             <Link to='/team/LAGalaxy'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/ysyysr1420227188.png/tiny' 
-                onClick={() => handleClick('LA Galaxy')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/lagalaxy.png'
+                    onClick={() => handleClick('LA Galaxy')}
                 />
             </Link>    
                 
             <Link to='/team/LAFC'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/7nbj2a1602103638.png/tiny'
-                onClick={() => handleClick('Los Angeles FC')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/lafc.png'
+                    onClick={() => handleClick('Los Angeles FC')}
                 />
             </Link>    
                 
             <Link to='/team/MinnesotaUnited'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/vl67t41492453433.png/tiny'
-                onClick={() => handleClick('Minnesota Utd')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/minnesota.png'
+                    onClick={() => handleClick('Minnesota Utd')}
                 />
             </Link>    
                 
             <Link to='/team/PortlandTimber'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/skm30j1557953559.png/tiny'
-                onClick={() => handleClick('Portland Timbers')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/portland.png'
+                    onClick={() => handleClick('Portland Timbers')}
                 />
             </Link>    
                 
             <Link to='/team/RealSaltLake'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/kkjlfa1556488022.png/tiny'
-                onClick={() => handleClick('Real Salt Lake')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/rsl.png'
+                    onClick={() => handleClick('Real Salt Lake')}
                 />
             </Link>    
                 
             <Link to='/team/SanJoseEarthquakes'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/xyrqqt1420781048.png/tiny'
-                onClick={() => handleClick('San Jose')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/sanjose.png'
+                    onClick={() => handleClick('San Jose')}
                 />
             </Link>    
                 
             <Link to='/team/SeattleSounders'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/vpyprt1473536493.png/tiny' 
-                onClick={() => handleClick('Seattle')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/seattle.png'
+                    onClick={() => handleClick('Seattle')}
                 />
             </Link>    
                 
             <Link to='/team/SportingKC'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/tqupxw1473536504.png/tiny'
-                onClick={() => handleClick('Sporting KC')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/skc.png'
+                    onClick={() => handleClick('Sporting KC')}
                 />
             </Link>    
                 
             <Link to='/team/StLouisCitySC'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/22wanp1674118724.png/tiny'
-                onClick={() => handleClick('St. Louis')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/stlouis.png'
+                    onClick={() => handleClick('St. Louis')}
                 />
             </Link>    
                 
             <Link to='/team/VancouverWhitecaps'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/tpwxpy1473536521.png/tiny' 
-                onClick={() => handleClick('Vancouver')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/vancouver.png'
+                    onClick={() => handleClick('Vancouver')}
                 />
             </Link>    
                 
             <Link to='/team/AtlantaUnited'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/ej091x1602103070.png/tiny'
-                onClick={() => handleClick('Atlanta Utd')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/atlanta.png'
+                    onClick={() => handleClick('Atlanta Utd')}
                 />
             </Link>    
                 
             <Link to='/team/CharlotteFC'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/b6p4uz1595434047.png/tiny'
-                onClick={() => handleClick('Charlotte')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/charlotte.png'
+                    onClick={() => handleClick('Charlotte')}
                 />
             </Link>    
                 
             <Link to='/team/ChicagoFire'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/8xuc781639493166.png/tiny'
-                onClick={() => handleClick('Chicago Fire')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/chicago.png'
+                    onClick={() => handleClick('Chicago Fire')}
                 />
             </Link>    
                 
             <Link to='/team/ColumbusCrew'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/dzs8cp1629059854.png/tiny'
-                onClick={() => handleClick('Columbus Crew')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/columbus.png'
+                    onClick={() => handleClick('Columbus Crew')}
                 />
             </Link>    
                 
             <Link to='/team/FCCincinnati'>    
-                <img src='https://www.thesportsdb.com/images/media/team/badge/47ve8t1555084948.png/tiny'
-                onClick={() => handleClick('FC Cincinnati')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/cincinnati.png'
+                    onClick={() => handleClick('FC Cincinnati')}
                 />
             </Link>
             
             <Link to='/team/DCUnited'> 
-                <img src='https://www.thesportsdb.com/images/media/team/badge/uwvsyt1467462609.png/tiny'
-                onClick={() => handleClick('D.C. United')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/dc.png'
+                    onClick={() => handleClick('D.C. United')}
                 />
             </Link>    
                 
             <Link to='/team/InterMiami'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/m4it3e1602103647.png/tiny'
-                onClick={() => handleClick('Inter Miami')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/miami.png'
+                    onClick={() => handleClick('Inter Miami')}
                 />
             </Link>    
                 
             <Link to='/team/CFMontreal'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/livdl31679344925.png/tiny' 
-                onClick={() => handleClick('CF Montréal')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/montreal.png'
+                    onClick={() => handleClick('CF Montréal')}
                 />
             </Link>    
                 
             <Link to='/team/NashvilleSC'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/znrwt71602103062.png/tiny' 
-                onClick={() => handleClick('Nashville')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/nashville.png'
+                    onClick={() => handleClick('Nashville')}
                 />
             </Link>   
             
             <Link to='/team/NewEnglandRevolution'> 
-                <img src='https://www.thesportsdb.com/images/media/team/badge/1ula2l1639493143.png/tiny' 
-                onClick={() => handleClick('New England')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/newengland.png'
+                    onClick={() => handleClick('New England')}
                 />
             </Link>    
 
             <Link to='/team/NewYorkCityFC'> 
-                <img src='https://www.thesportsdb.com/images/media/team/badge/li4y2o1534575820.png/tiny'
-                onClick={() => handleClick('NYCFC')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/nycfc.png'
+                    onClick={() => handleClick('NYCFC')}
                 />
             </Link>    
                 
             <Link to='/team/NewYorkRedbulls'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/suytvy1473536462.png/tiny' 
-                onClick={() => handleClick('NY Red Bulls')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/nyredbulls.png'
+                    onClick={() => handleClick('NY Red Bulls')}
                 />
             </Link>
 
             <Link to='/team/OrlandoCity'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/qyppxw1423832326.png/tiny'
-                onClick={() => handleClick('Orlando City')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/orlando.png'
+                    onClick={() => handleClick('Orlando City')}
                 />
             </Link>    
                 
             <Link to='/team/PhiladelphiaUnion'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/gyznyo1602103682.png/tiny' 
-                onClick={() => handleClick('Philadelphia')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/philadelphia.png'
+                    onClick={() => handleClick('Philadelphia')}
                 />
             </Link>    
             
             <Link to='/team/TorontoFC'>     
-                <img src='https://www.thesportsdb.com/images/media/team/badge/rsxyrr1473536512.png/tiny'
-                onClick={() => handleClick('Toronto FC')}
+                <img 
+                    className='team-logo'
+                    src='/logos/full-size/torontofc.png'
+                    onClick={() => handleClick('Toronto FC')}
                 />
             </Link>
         </div>
