1c41f58d0c46e6bdd2dd89776df251618a962a8f
Nick Hoefle <Nickhoefle@gmail.com>
2024-04-13 22:11:39 -0500

    Removed redundant React boilerplate files.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
