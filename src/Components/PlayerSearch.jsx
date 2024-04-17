2148d54767e4c0e1021e68a8ea18bcf7255d59e9
Nick Hoefle <Nickhoefle@gmail.com>
2024-04-16 22:43:33 -0500

    Removed search button and form to rely only on suggestions from search.

--- src/Components/PlayerSearch.jsx
+++ src/Components/PlayerSearch.jsx
@@ -27,29 +27,29 @@ const PlayerSearch = ({ handlePlayerSearch }) => {
     return (
         <div className="flex justify-center lg:p-4">
             <div className="text-white text-center lg:w-1/2">    
-                    <h1 className="text-2xl text-white pb-4">Player Search</h1>
-                    <input
-                        className="text-black rounded-full p-3 lg:w-1/3 text-lg"                
-                        placeholder="Search for an MLS Player"
-                        value={searchBarText}
-                        onChange={handleChange}
-                    />
-                    <br />
-                    {suggestedPlayers.length > 0 && (
-                        <div className='lg:flex justify-center'>
-                            <ul className="mt-2 bg-white text-black rounded lg:w-1/3">
-                                <li className='font-bold underline'>Suggested Players</li>
-                                {suggestedPlayers.map((suggestedPlayer, index) => (
-                                    <li 
-                                        key={index} 
-                                        className="cursor-pointer p-1 lg:hover:bg-green-700 hover:text-white text-lg"
-                                        onClick={ () => handleSuggestionClick(suggestedPlayer) }                            
-                                    >
-                                        {suggestedPlayer}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                <h1 className="text-2xl text-white pb-4">Player Search</h1>
+                <input
+                    className="text-black rounded-full p-3 lg:w-1/3 text-lg"                
+                    placeholder="Search for an MLS Player"
+                    value={searchBarText}
+                    onChange={handleChange}
+                />
+                <br />
+                {suggestedPlayers.length > 0 && (
+                    <div className='lg:flex justify-center'>
+                        <ul className="mt-2 bg-white text-black rounded lg:w-1/3">
+                            <li className='font-bold underline'>Suggested Players</li>
+                            {suggestedPlayers.map((suggestedPlayer, index) => (
+                                <li 
+                                    key={index} 
+                                    className="cursor-pointer p-1 lg:hover:bg-green-700 hover:text-white text-lg"
+                                    onClick={ () => handleSuggestionClick(suggestedPlayer) }                            
+                                >
+                                    {suggestedPlayer}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                     )}         
             </div>
         </div>
